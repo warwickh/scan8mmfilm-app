8e14107d2cf3da1db435e86fb684af8df19d0752
warwick.harris <warwick.harris@yahoo.com.au>
2024-02-03 11:40:18 +1100

    major change to ratio(masked) for testing-working add dup processing

--- DupFrameDetector.py
+++ DupFrameDetector.py
@@ -2,9 +2,9 @@ import numpy as np
 from PIL import Image
 from tensorflow.keras.preprocessing import image
 import csv
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import matplotlib.animation as animation
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
+#import matplotlib.animation as animation
 
 from keras.applications.vgg16 import VGG16
 from sklearn.metrics.pairwise import cosine_similarity
@@ -16,6 +16,7 @@ class DupFrameDetector:
     def __init__(self):
         self.vgg16 = VGG16(weights='imagenet', include_top=False, pooling='max', input_shape=(224, 224, 3))
         self.prepare_model()
+        self.loaded = False
 
     def prepare_model(self):
         for model_layer in self.vgg16.layers:
@@ -47,61 +48,36 @@ class DupFrameDetector:
         h_img = cv2.hconcat([img1, img2])
         return h_img
 
-    def show_seq(self, first_image,second_image):
-        fig, ax = plt.subplots()
-        img1 = [ax.imshow(cv2.imread(first_image), animated=True)]
-        img2 = [ax.imshow(cv2.imread(second_image), animated=True)]
-        ims=[img1,img2]
-        ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,repeat_delay=0)
+    #def show_seq(self, first_image,second_image):
+    #    fig, ax = plt.subplots()
+    #    img1 = [ax.imshow(cv2.imread(first_image), animated=True)]
+    #    img2 = [ax.imshow(cv2.imread(second_image), animated=True)]
+    #    ims=[img1,img2]
+    #    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,repeat_delay=0)
+    #    plt.show()
 
-        # To save the animation, use e.g.
-        #
-        # ani.save("movie.mp4")
-        #
-        # or
-        #
-        # writer = animation.FFMpegWriter(
-        #     fps=15, metadata=dict(artist='Me'), bitrate=1800)
-        # ani.save("movie.mp4", writer=writer)
-
-        plt.show()
-
-    def combine(csv1, csv2):
-        if not os.path.exists(csv1) or not os.path.exists(csv2):
-            return
-        else:
-            with open(os.path.expanduser("~/combined_dups.csv"),'w',newline='\n') as writeFile:
-                reader1 = csv.reader(open(csv1))
-                reader2 = csv.reader(open(csv2))
-                writer = csv.writer(writeFile)
-                for row1 in reader1:
-                    row2 = next(reader2)
-                    outRow = f"{row1} {row2}"
-                    outRow = row1+row2
-                    print(outRow)
-                    writer.writerow(outRow)
-
-    def createDupLog(self, folder):
+    def createDupLog(self, folder, force=False):
         #roll = "roll6"
         #folder = os.path.expanduser(f"~/scanframes/crop/{roll}")
         os.chdir(folder)
         fileList = sorted(glob.glob('frame*.jpg'))
         lastImage = None
         #logPath = os.path.expanduser(f"~/dups/log.csv")
-        logPath = os.path.join(folder,f"frame_dups.csv")
+        self.logPath = os.path.join(folder,f"frame_dups.csv")
         #transPath = os.path.expanduser(f"~/{roll}_transforms.csv")
-        #if not os.path.exists(logPath):
-        with open(logPath,'w') as logFile:
-            for fn in fileList:
-                print(f"{fn}")
-                currentImage = os.path.join(folder,fn)
-                if lastImage:
-                    similarity_score = self.get_similarity_score(lastImage, currentImage)
-                    logFile.write(f"{lastImage},{currentImage},{similarity_score[0]} \n")
-                    lastImage = currentImage
-                    print(f"{lastImage} {currentImage} similarity score {similarity_score[0]}")
-                else:
-                    lastImage = currentImage
+        if not os.path.exists(self.logPath) or force:
+            with open(self.logPath,'w') as logFile:
+                for fn in fileList:
+                    print(f"{fn}")
+                    currentImage = os.path.join(folder,fn)
+                    if lastImage:
+                        similarity_score = self.get_similarity_score(lastImage, currentImage)
+                        logFile.write(f"{lastImage},{currentImage},{similarity_score[0]} \n")
+                        lastImage = currentImage
+                        print(f"{lastImage} {currentImage} similarity score {similarity_score[0]}")
+                    else:
+                        lastImage = currentImage
+        self.getData()
         #else:
         #    combine(logPath, transPath)
         #else:
@@ -110,6 +86,34 @@ class DupFrameDetector:
         #      for row in reader_obj: 
         #          print(row)
         #          show_seq(row[0],row[1])
+    
+    def getNextRow(self):
+        #print(f"loaded {self.loaded}")
+        if not self.loaded:
+            print("Not loaded")
+            return None
+        return next(self.logIterator)
+    
+    def getNextImages(self):
+        row = self.getNextRow()
+        if not row:
+            return None, None, None
+        #print(row)
+        first_image = self.load_image(row['img1'])
+        second_image = self.load_image(row['img2'])
+        similarity = row['similarity']
+        return first_image, second_image, similarity
+
+    def getData(self):
+        if not os.path.exists(self.logPath):
+            print(f"No file {self.logPath}")
+            self.loaded = False
+        else:
+            with open(self.logPath,'r') as logFile:
+                dict_reader = csv.DictReader(logFile,fieldnames=['img1','img2','similarity'])
+                self.logData = list(dict_reader)
+            self.logIterator = iter(self.logData)
+            self.loaded = True
 
 if __name__ == "__main__":
     roll = "roll6"
